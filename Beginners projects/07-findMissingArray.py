afbed37bcaf592a7b3417a7453a1a19c9c87add6
Nathan Bruniquel <165512298+Brynhyldr@users.noreply.github.com>
2026-07-26 14:48:13 +0000

    ok

---
+++ Beginners projects/07-findMissingArray.py
@@ -0,0 +1,13 @@
+listOfNumbers = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 13, 14, 16]
+
+def findMissingArray(listOfNumbers):
+    answer = []
+    for i in range(len(listOfNumbers)):
+        try:
+            if listOfNumbers[i] != listOfNumbers[i+1]-1:
+                answer.append(listOfNumbers[i]+1)
+        except:
+            print("end of analysis")
+    return answer
+
+print(findMissingArray(listOfNumbers))
