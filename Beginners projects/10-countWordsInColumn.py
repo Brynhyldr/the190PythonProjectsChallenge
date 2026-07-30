cf1ab1f859c97298405bddfb19efea7f93344008
Brynhyldr <nathan.bruniquel@gmail.com>
2026-07-30 15:15:41 +0200

    ok

--- Beginners projects/10-countWordsInColumn.py
+++ Beginners projects/10-countWordsInColumn.py
@@ -1,4 +1,13 @@
 import pandas as pd
 data = pd.read_csv("https://raw.githubusercontent.com/amankharwal/Website-data/master/articles.csv", encoding = 'latin1')
-print(data.head())
 
+def addWordCounter(db, column):
+    numberOfWords = []
+    for line in range(len(db[column])):
+        numberOfWords.append(len(str(db[column][line]).split()))
+    db["Nb of Words"] = numberOfWords
+    return db
+
+countedDb = addWordCounter(data, "Article")
+
+print(countedDb.head())
